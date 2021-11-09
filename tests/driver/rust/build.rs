b90d7c03ea3bdd9dc465daee87a40d650ef222b0
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-11-09 16:16:47 +0100

    Speed up the build test of the rust test suite when using the build-time feature
    Generate the Rust code in parallel.
    
    That brings down the code generation time for cargo test -p
    test-driver-rust --features build time in build.rs from 30 seconds down
    to 6 on my machine.
    
    Compared to the macro build this saves ~10 seconds on my machine.

--- tests/driver/rust/build.rs
+++ tests/driver/rust/build.rs
@@ -8,49 +8,73 @@
     Please contact info@sixtyfps.io for more information.
 LICENSE END */
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+use rayon::prelude::*;
 
 fn main() -> std::io::Result<()> {
     let mut generated_file = std::fs::File::create(
         Path::new(&std::env::var_os("OUT_DIR").unwrap()).join("generated.rs"),
     )?;
 
-    for testcase in test_driver_lib::collect_test_cases()? {
-        println!("cargo:rerun-if-changed={}", testcase.absolute_path.display());
-        let mut module_name = testcase.identifier();
-        if module_name.starts_with(|c: char| !c.is_ascii_alphabetic()) {
-            module_name.insert(0, '_');
-        }
-        writeln!(generated_file, "#[path=\"{0}.rs\"] mod r#{0};", module_name)?;
-        let source = std::fs::read_to_string(&testcase.absolute_path)?;
+    let compile_results: Vec<std::io::Result<Vec<PathBuf>>> =
+        test_driver_lib::collect_test_cases()?
+            .par_iter()
+            .map(|testcase| {
+                let mut dependencies = Vec::new();
+                dependencies.push(testcase.absolute_path.clone());
+                let mut module_name = testcase.identifier();
+                if module_name.starts_with(|c: char| !c.is_ascii_alphabetic()) {
+                    module_name.insert(0, '_');
+                }
+                let source = std::fs::read_to_string(&testcase.absolute_path)?;
 
-        let mut output = std::fs::File::create(
-            Path::new(&std::env::var_os("OUT_DIR").unwrap()).join(format!("{}.rs", module_name)),
-        )?;
+                let mut output = std::fs::File::create(
+                    Path::new(&std::env::var_os("OUT_DIR").unwrap())
+                        .join(format!("{}.rs", module_name)),
+                )?;
 
-        #[cfg(not(feature = "build-time"))]
-        if !generate_macro(&source, &mut output, testcase)? {
-            continue;
-        }
-        #[cfg(feature = "build-time")]
-        generate_source(&source, &mut output, testcase)?;
+                #[cfg(not(feature = "build-time"))]
+                if !generate_macro(&source, &mut output, testcase, &mut dependencies)? {
+                    return Ok(dependencies);
+                }
+                #[cfg(feature = "build-time")]
+                generate_source(&source, &mut output, testcase)?;
 
-        for (i, x) in test_driver_lib::extract_test_functions(&source)
-            .filter(|x| x.language_id == "rust")
-            .enumerate()
-        {
-            write!(
-                output,
-                r"
+                for (i, x) in test_driver_lib::extract_test_functions(&source)
+                    .filter(|x| x.language_id == "rust")
+                    .enumerate()
+                {
+                    write!(
+                        output,
+                        r"
 #[test] fn t_{}() -> Result<(), Box<dyn std::error::Error>> {{
     sixtyfps_rendering_backend_testing::init();
     {}
     Ok(())
 }}",
-                i,
-                x.source.replace('\n', "\n    ")
-            )?;
+                        i,
+                        x.source.replace('\n', "\n    ")
+                    )?;
+                }
+
+                Ok(dependencies)
+            })
+            .collect();
+
+    for result in compile_results {
+        let dependencies = result?;
+        for dep in &dependencies {
+            println!("cargo:rerun-if-changed={}", dep.display());
         }
+    }
+
+    for testcase in test_driver_lib::collect_test_cases()? {
+        let mut module_name = testcase.identifier();
+        if module_name.starts_with(|c: char| !c.is_ascii_alphabetic()) {
+            module_name.insert(0, '_');
+        }
+        writeln!(generated_file, "#[path=\"{0}.rs\"] mod r#{0};", module_name)?;
     }
 
     // By default resources are embedded. The WASM example builds provide test coverage for that. This switch
@@ -67,7 +91,8 @@ fn main() -> std::io::Result<()> {
 fn generate_macro(
     source: &str,
     output: &mut std::fs::File,
-    testcase: test_driver_lib::TestCase,
+    testcase: &test_driver_lib::TestCase,
+    dependencies: &mut Vec<PathBuf>,
 ) -> Result<bool, std::io::Error> {
     if source.contains("\\{") {
         // Unfortunately, \{ is not valid in a rust string so it cannot be used in a sixtyfps! macro
@@ -85,7 +110,8 @@ fn generate_macro(
         output.write_all(abs_path.to_string_lossy().as_bytes())?;
         output.write_all(b"\"#]\n")?;
 
-        println!("cargo:rerun-if-changed={}", abs_path.to_string_lossy());
+        //println!("cargo:rerun-if-changed={}", abs_path.to_string_lossy());
+        dependencies.push(abs_path);
     }
     let mut abs_path = testcase.absolute_path.clone();
     abs_path.pop();
@@ -101,13 +127,12 @@ fn generate_macro(
 fn generate_source(
     source: &str,
     output: &mut std::fs::File,
-    testcase: test_driver_lib::TestCase,
+    testcase: &test_driver_lib::TestCase,
 ) -> Result<(), std::io::Error> {
     use sixtyfps_compilerlib::{diagnostics::BuildDiagnostics, *};
 
-    let include_paths = test_driver_lib::extract_include_paths(source)
-        .map(std::path::PathBuf::from)
-        .collect::<Vec<_>>();
+    let include_paths =
+        test_driver_lib::extract_include_paths(source).map(PathBuf::from).collect::<Vec<_>>();
 
     let mut diag = BuildDiagnostics::default();
     let syntax_node = parser::parse(source.to_owned(), Some(&testcase.absolute_path), &mut diag);
